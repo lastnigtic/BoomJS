62b6a2d93c848b3d0f502cc2ca7bd0e359ccc928
lastnigtic <1113594660@qq.com>
2017-07-20 22:27:07 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# BoomJS
-imgBoom
+## boomJS
+
+@(boomJS)[v1.0]
+### 一、简介
+- 基于jquery
+- 使用ES6语法
+
+### 二、demo
+#### 1、使用
+    $([selector]).imgBoom(option);
+#### 2、参数说明
+- columns：分割列数，数字
+- row：分割行数，数字
+- isBlur： 是否使用模糊效果（分割太多会卡顿）
+- isBoom： 是否添加点击炸裂效果（会删除元素）
+- duration：动画时长
+#### 3、效果
+![boomjs.gif](https://raw.githubusercontent.com/lastnigtic/presentationPIC/master/boomJs/0.gif)
+    
---
+++ js/imgBoom.js
@@ -0,0 +1,163 @@
+	/*传入ID*/
+	(function(){
+		function imgBoom(option,img){
+			option = option || {};
+			this.img = $(img);
+			this.rows = option.rows || 8;
+			this.columns = option.columns || 8;
+			this.duration = option.duration || 1000;
+			this.isBlur = option.isBlur || false;
+			this.isBoom = option.isBoom || false;
+			this.isInit = true;
+			this.img.css('opacity',0);
+			this.img.load(() => {
+				this.height = this.img.height();
+				this.width = this.img.width();
+				this.init();
+			})
+		}
+		/**
+		 * 初始化
+		 */
+		 imgBoom.prototype.init = function(){
+		 	this.createPieces();
+		 	this.piecesBoom();
+		 	setTimeout(() => {
+		 		this.piecesRecover()
+		 	},0);
+		 	if(this.isBoom){
+		 		this.img.on('click',() => {
+		 			this.piecesBoom();
+		 		})
+		 	}
+		 }
+
+		/**
+		 * 创造块(单独负责创造小块)
+		 */
+		 imgBoom.prototype.createPieces = function(){
+		 	let id = this.img.attr('id');
+		 	this.pieces = [];
+
+			//存放pieces的块
+			this.children = $("<div></div>");
+			this.children.css({
+				'position': 'absolute',
+				'top': 0,
+				'left': 0,
+				'margin-left': this.img.prop('offsetLeft') + 'px',
+				'margin-top': this.img.prop('offsetTop') + 'px',
+				'z-index': 100,
+				'display': 'none'
+			});
+			this.children.attr('id', id+'Children');
+
+			// 创造piece
+			let pieceHeight = this.height / this.rows;
+			let pieceWidth = this.width / this.columns; 
+			let frag = document.createDocumentFragment();
+			let background = `url(${this.img.attr('src')}) no-repeat`;
+			let backgroundSize = `${this.width}px ${this.height}px`;
+			let transition = `all ${this.duration}ms ease-out`;
+			for(let i = 0; i < this.columns; i++){
+				let left = '-' + (pieceWidth * i) + 'px';
+				for( let j = 0; j < this.rows; j++){
+					let top = '-' + (pieceHeight * j) + 'px';
+					let child = $("<div class='boom-img-child'></div>");
+					child.css({
+						'position': 'absolute',
+						'display': 'inline-block',
+						'left': left.substring(1),
+						'top': top.substring(1),
+						'height': pieceHeight + 'px',
+						'width': pieceWidth + 'px',
+						'background': background,
+						'background-size':backgroundSize,
+						'background-position-x': left,
+						'background-position-y': top,
+						'transition': transition,
+					});
+					$(frag).append(child);
+					this.pieces.push(child);
+				}
+			};
+			this.children.append(frag);
+			this.children.insertAfter(this.img);
+		};
+
+		/**
+		 * 块爆炸,调整块的位置，让img隐藏，children显示
+		 */
+		 imgBoom.prototype.piecesBoom = function(){
+
+			// 点击图片(删除全部元素)
+			if(!this.isInit){
+				this.children.show();
+				setTimeout(() => {
+					this.img.remove();
+					this.children.remove();
+				},1.5*this.duration);
+			}
+
+			this.isInit = false;
+
+			// 调整块的位置
+			if(this.children){
+				this.children.css({
+					'margin-left': this.img.prop('offsetLeft') + 'px',
+					'margin-top': this.img.prop('offsetTop') + 'px'
+				})
+			}
+
+			setTimeout(() => {
+
+				$(this.pieces).each((idx, item) => {
+					let x = (100-(Math.random()*200)).toFixed() + 'px';
+					let y = (100-(Math.random()*200)).toFixed() + 'px';
+					let transformPosition = `translate3D(${x}, ${y} ,0) `;
+					let transformSize = `scale(${(Math.random()/2)})`;
+					let  blur;
+
+					if(this.isBlur){
+						blur = `blur(${(Math.random()*10).toFixed()}px)`;
+					}else{
+						blur = 'none';
+					}
+
+					item.css({
+						'border-radius':'50%',
+						'transform': transformPosition + transformSize,
+						'opacity': 0,
+						'filter': blur
+					});
+				});
+				this.img.css('opacity',0);
+			},0);
+		};
+
+		/**
+		 * 块恢复,children隐藏,img显示(opacity
+		 */
+		 imgBoom.prototype.piecesRecover = function(){	
+		 	this.children.show();
+		 	$(this.pieces).each((idx, item) => {
+		 		item.css({
+		 			'transform': 'none', 
+		 			'opacity': 1,
+		 			'filter':'none',
+		 			'border-radius': '0px'
+		 		})
+		 	});
+		 	setTimeout(()=>{
+		 		this.img.css('opacity',1);
+		 		this.children.hide();
+		 	},this.duration);
+		 };
+
+		 jQuery.fn.extend({
+		 	imgBoom: function(option){
+		 		new imgBoom(option,this[0]);
+		 		return this;
+		 	}
+		 });
+		})()
